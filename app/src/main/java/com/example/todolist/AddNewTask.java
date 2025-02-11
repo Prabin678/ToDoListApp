bba24116354d14b26ffc390ee936815809a09b20
Prabin678 <kumarpattnaikprabin@gmail.com>
2025-02-11 23:44:56 +0530

    Final Out put

---
+++ app/src/main/java/com/example/todolist/AddNewTask.java
@@ -0,0 +1,53 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.todolist.Utils.DataBaseHelper;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class AddNewTask extends BottomSheetDialogFragment {
+
+    private static final String TAG = "AddNewTask";
+    private EditText mEditText;
+    private Button mSaveButton;
+    private DataBaseHelper myDB;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.add_new_task,container,false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mEditText = view.findViewById(R.id.editText);
+        mSaveButton = view.findViewById(R.id.addButton);
+
+        myDB = new DataBaseHelper((getActivity()));
+
+        boolean isUpdate = false;
+
+        Bundle bundle = getArguments();
+        if(bundle != null){
+            isUpdate = true;
+            String task = bundle.getString("task");
+            mEditText.setText(task);
+            if(task.length() > 0){
+                mSaveButton.setEnabled(false);
+            }
+        }
+        
+
+    }
+}
